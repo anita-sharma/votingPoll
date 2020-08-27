efb4447f08d87c335e201f9d96a095039207620a
anita-sharma <2016uce1171@mnit.ac.in>
2020-08-27 15:15:03 +0530

    Updates

---
+++ precache-manifest.903a86ec481fa366b89e35204528a52a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622958964d637d41ca5353838cfb63e5",
+    "url": "/votingPoll/index.html"
+  },
+  {
+    "revision": "14d7aa148fe575bcfab5",
+    "url": "/votingPoll/static/css/main.767f4197.chunk.css"
+  },
+  {
+    "revision": "200cfd85f661018dcf34",
+    "url": "/votingPoll/static/js/2.abb41b11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/votingPoll/static/js/2.abb41b11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d7aa148fe575bcfab5",
+    "url": "/votingPoll/static/js/main.c621891f.chunk.js"
+  },
+  {
+    "revision": "67e8ff37c8bfb3c20adf",
+    "url": "/votingPoll/static/js/runtime-main.de23a9a7.js"
+  }
+]);
